af91b103863af250718f9b628361c0c3b6b7e7e2
XyYpower <lilizhi134@gmail.com>
2026-09-23 10:08:14 +0800

    chore: 删除调试脚本

--- scripts/tmp-parse-check.ts
+++
@@ -1,6 +0,0 @@
-import { parseReviewText } from "../src/domain/review/parse";
-
-const lines = parseReviewText("显卡  某卡 12,999\n电源  某电源 ¥1,299.5");
-for (const l of lines) {
-  console.log(JSON.stringify({ cat: l.category, name: l.name, price: l.priceCents, skipped: l.skipped, reason: l.skipReason }));
-}
